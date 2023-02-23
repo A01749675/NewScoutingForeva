ef05c500668f16119fa349d2f8e01389ba7e8e9a
Iker_Fuentes <117104395+A01749675@users.noreply.github.com>
2023-02-22 19:42:05 -0600

    Test

--- scripts/templates/scripts/test.js
+++ scripts/templates/scripts/test.js
@@ -1,16 +1,33 @@
-var mysql = require('mysql2');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Caikfure3472",
-  database: "Scouting"
-});
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT user,password FROM scouters", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+
+
+function connect(){
+    var mysql = require('mysql2');
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Caikfure3472",
+        database: "Scouting"
+      });
+    return con
+}
+
+
+function log_in(user,password){
+    var mysql = require('mysql2');
+    con =connect();
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT user,password FROM scouters WHERE user = "+mysql.escape(user), function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            
+            if(user==result[0].user && password==result[0].password){
+                console.log("access")
+            }
+        });
+      });
+
+}
+log_in("scout","root")
